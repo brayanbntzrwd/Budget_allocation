b258db0e48219838a055cb4602b96c484155d1db
brayanbntzrwd <brayanbntzrwd@gamil.com>
2023-03-06 14:08:50 -0600

    Editable budget value

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,41 @@
 import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext'; 
+import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget, currency } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: {currency}{budget}</span>
-        </div>
-    );
+	const { budget, currency, dispatch, expenses } = useContext(AppContext);
+	const totalExpenses = expenses.reduce((total, item) => {
+		return (total += item.cost);
+	}, 0);
+	const chgBudget = (val) => {
+		if(val>20000){
+			alert('The budget value cannot be higher than 20,000')
+			val = budget;
+		}
+		if(val<totalExpenses){
+			alert('You cannot reduces the budget value lower than the spending');
+			val = totalExpenses;
+		}
+		dispatch({
+			type: 'SET_BUDGET',
+			payload: val
+		});
+	};
+	return (
+		<div className='alert alert-secondary'>
+			<span>Budget: {currency}
+
+				<input
+					required='required'
+					type='number'
+					id='budget'
+					min='0'
+					value={budget}
+					style={{ width:180 }}
+					step='10'
+					onChange={(event) => chgBudget(event.target.value)}>
+				</input>
+			</span>
+		</div>
+	);
 };
 export default Budget;
 
